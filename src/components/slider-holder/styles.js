870baba797ed9ea2b22f450c8107517ca8b6af09
Jeff McAmmond <jeffmcammond@gmail.com>
2019-06-03 22:27:57 -0600

    remove opacity

--- src/components/slider-holder/styles.js
+++ src/components/slider-holder/styles.js
@@ -12,41 +12,34 @@ const Slide = styled.label`
     left: 0;
     width: 100%;
     cursor: pointer;
-    opacity: 1;
     transition: all 0.2s ease;
     display: block;
-    opacity: 0;
     position: absolute;
     z-index: -1;
     transform: translate(0) scale(0.65);
 
     ${props => props.main && css`
-        opacity: 1;
         position: relative;
         z-index: 2;
         transform: translate(0) scale(1);
     `}
 
     ${props => props.direction === 'left-1' && css`
-        opacity: 1;
         z-index: 1;
         transform: translate(-100px) scale(0.85);
     `}
 
     ${props => props.direction === 'left-2' && css`
-        opacity: 1;
         z-index: 0;
         transform: translate(-210px) scale(0.65);
     `}
 
     ${props => props.direction === 'right-1' && css`
-        opacity: 1;
         z-index: 1;
         transform: translate(100px) scale(0.85);
     `}
 
     ${props => props.direction === 'right-2' && css`
-        opacity: 1;
         z-index: 0;
         transform: translate(210px) scale(0.65);
     `}
